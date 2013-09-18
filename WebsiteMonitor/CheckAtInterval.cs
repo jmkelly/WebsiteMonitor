d4bd59e3d059afffdd07c41263451c2fd6a99107
James Kelly <james@midway-pl.com.au>
2013-09-18 11:27:57 +1000

    initial commmit

---
+++ WebsiteMonitor/CheckAtInterval.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Timer = System.Timers.Timer;
+using EasyHttp.Http;
+using System.Windows.Media;
+
+namespace WebsiteMonitor
+{
+    public class CheckWebsiteAtInterval
+    {
+        private readonly Rectangle _statusIndicator;
+        private readonly TextBlock _lastUpdatedTextBlock;
+        private readonly TextBlock _messageTextBox;
+        private readonly Dispatcher _threadDispatcher;
+        private string _url;
+        private string _textToMatch;
+        private Timer _timer;
+
+        public CheckWebsiteAtInterval(Rectangle statusIndicator, TextBlock lastUpdatedTextBlock, TextBlock messageTextBox, Dispatcher threadDispatcher)
+        {
+            _statusIndicator = statusIndicator;
+            _lastUpdatedTextBlock = lastUpdatedTextBlock;
+            _messageTextBox = messageTextBox;
+            _threadDispatcher = threadDispatcher;
+        }
+
+        public void Start(int interval, string address, string textToMatch)
+        {
+            _url = address;
+            _textToMatch = textToMatch;
+            _timer = new Timer {Interval = TimeSpan.FromSeconds(interval).TotalMilliseconds};
+            _timer.Elapsed += Check;
+            _timer.Start();
+        }
+
+        private void Check(object sender, ElapsedEventArgs e)
+        {
+            var http = new HttpClient();
+            var result = false;
+            string message = "";
+
+            try
+            {
+                var response = http.Get(_url);
+                result =  response.RawText.Contains(_textToMatch);
+                message += result ?  string.Format(@"Message:  ""{0}"" found in page", _textToMatch) : string.Format(@"Message:  ""{0}"" not found in page", _textToMatch);
+
+            }
+            catch (Exception exception)
+            {
+                message += exception.Message;
+            }
+
+
+            _threadDispatcher.Invoke(DispatcherPriority.Input, new Action(() =>
+            {
+                _statusIndicator.Fill = result
+                    ? new SolidColorBrush(Colors.Chartreuse)
+                    : new SolidColorBrush(Colors.Red);
+                _lastUpdatedTextBlock.Text = string.Format("Last tested at {0}", DateTime.Now);
+                _messageTextBox.Text = message;
+
+            }));
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+    }
+}
---
+++ WebsiteMonitor/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using EasyHttp.Http;
+
+namespace WebsiteMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly CheckWebsiteAtInterval _checker;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _checker = new CheckWebsiteAtInterval(StatusIndicator,LastTestAtTextBox,MessageTextBox, Dispatcher.CurrentDispatcher);
+            SiteTextBox.Text = Properties.Settings.Default["address"].ToString();
+            TextToMatchTextBox.Text = Properties.Settings.Default["textToMatch"].ToString();
+            RecurIntervalTextBox.Text = Properties.Settings.Default["interval"].ToString();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Properties.Settings.Default["address"] = SiteTextBox.Text;
+            Properties.Settings.Default["textToMatch"] = TextToMatchTextBox.Text;
+            Properties.Settings.Default["interval"] = RecurIntervalTextBox.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {            var address = SiteTextBox.Text;
+            var textToMatch = TextToMatchTextBox.Text;
+            MessageTextBox.Text = "Message: ";
+
+            var http = new HttpClient();
+            bool result = false;
+            try
+            {
+                var response = http.Get(address);
+                result = response.RawText.Contains(textToMatch);
+                MessageTextBox.Text = result ? string.Format(@"Message:  ""{0}"" found in page", textToMatch) : string.Format(@"Message:  ""{0}"" not found in page", textToMatch);
+            }
+            catch (Exception ex)
+            {
+                MessageTextBox.Text = "Message: " + ex.Message;
+            }
+
+                StatusIndicator.Fill = result ? new SolidColorBrush(Colors.Chartreuse) : new SolidColorBrush(Colors.Red);
+                LastTestAtTextBox.Text = string.Format("Last tested at {0}", DateTime.Now);
+
+        }
+
+        private void StartTimeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var address = SiteTextBox.Text;
+            var textToMatch = TextToMatchTextBox.Text;
+            int interval = Convert.ToInt32(RecurIntervalTextBox.Text);
+
+            if ((string) StartTimerButton.Content == "Start Timer")
+            {
+                _checker.Start(interval, address, textToMatch);
+                StartTimerButton.Content = "Stop Timer";
+                DisableButtons();
+            }
+            else
+            {
+                _checker.Stop();
+                StartTimerButton.Content = "Start Timer";
+                EnableButtons();
+            }
+        }
+
+        private void DisableButtons()
+        {
+            SiteTextBox.IsEnabled = false;
+            TextToMatchTextBox.IsEnabled= false;
+            RecurIntervalTextBox.IsEnabled = false;
+        }
+        
+        private void EnableButtons()
+        {
+            SiteTextBox.IsEnabled = true;
+            TextToMatchTextBox.IsEnabled = true;
+            RecurIntervalTextBox.IsEnabled = true;
+        }
+    }
+
+   
+}
+    
